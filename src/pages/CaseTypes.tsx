24bcee45a5cb7768308cb7632a17b2c4d6346827
gencogedik <87668301+gencogedik@users.noreply.github.com>
2025-07-16 11:44:01 +0300

    Update CaseTypes.tsx

--- src/pages/CaseTypes.tsx
+++ src/pages/CaseTypes.tsx
@@ -1,4 +1,5 @@
 
+import { supabase } from '@/integrations/supabase/client';
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Header from '@/components/Header';
